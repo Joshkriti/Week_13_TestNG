3dba56eacf217bccb1e4a51baa5d756e26b6a578
Joshkriti <Komalkanji13@gmail.com>
2024-05-30 12:29:04 +0100

    First push

---
+++ You-Store-Annotation/src/main/java/yourstore/annotation/App.java
@@ -0,0 +1,13 @@
+package yourstore.annotation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
